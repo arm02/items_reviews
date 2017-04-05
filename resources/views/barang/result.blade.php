2d6fc40a5693f876678239c085f0dd3efa852f0e
andy <andy.natalino@gmail.com>
2017-04-05 14:37:49 +0700

    nambah dropdown pada search di index, beranda dan result

--- resources/views/barang/result.blade.php
+++ resources/views/barang/result.blade.php
@@ -7,9 +7,20 @@
 
 @section('beranda')
 <form action={{ url('search') }} method="GET" class="navbar-form navbar-left">
-    <div class="form-group">
-      <input type="text" name="q" class="form-control" value="{{ $search }}" placeholder="Search">
+  <div class="input-group">
+    <div class="input-group-btn">
+      <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action <span class="caret"></span></button>
+      <ul class="dropdown-menu">
+        <li><a href="#">Lorem</a></li>
+        <li><a href="#">ipsum</a></li>
+        <li><a href="#">dolor</a></li>
+        <li><a href="#">Lorem</a></li>
+        <li role="separator" class="divider"></li>
+        <li><a href="#">amet</a></li>
+      </ul>
     </div>
+    <input type="text" name="q" class="form-control" value="{{ $search }}" placeholder="Search">
+  </div>
 </form>
 @endsection
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,18 +24,44 @@
     <img src="{{url('images/google-web-search-xxl.png')}}" width="200" height="">
     </center>
 
-      <form action={{ url('search') }} method="GET" class="form-signin">
-      <div class="center" style="width:500px; margin:0 auto;"> 
-      <center>
-      <br>
-        <input type="text" name="q" class="form-control" placeholder="Search Here" required autofocus>
-        <br>
-        <button class="btn btn-lg btn-primary" type="submit">Search</button>
+    <!-- <form action={{ url('search') }} method="GET" class="form-signin">
+      <div class="center" style="width:500px; margin:0 auto;">
+        <center>
+          <br>
+          <input type="text" name="q" class="form-control" placeholder="Search Here" required autofocus>
+          <br>
+          <button class="btn btn-lg btn-primary" type="submit">Search</button>
         </center>
-     </div>
-    </div> <!-- /container -->
+      </div>
     </div>
-      </form>
+  </div>
+</form> -->
 
-    @endsection
-
+<div class="container">
+  <div class="row">
+    <form action={{ url('search') }} method="GET" class="form-signin">
+      <div class="center" style="width:500px; margin:0 auto;">
+        <center>
+          <div class="input-group">
+            <div class="input-group-btn">
+              <button name="cat" type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action <span class="caret"></span></button>
+              <ul class="dropdown-menu">
+                <li><a href="#">Lorem</a></li>
+                <li><a href="#">ipsum</a></li>
+                <li><a href="#">dolor</a></li>
+                <li><a href="#">Lorem</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">amet</a></li>
+              </ul>
+            </div>
+            <input type="text" name="q" class="form-control" placeholder="Search Here" required autofocus>
+            <br>
+          </center>
+          <center>
+            <br>
+            <button class="btn btn-lg btn-primary" type="submit">Search</button>
+          </center>
+        </form>
+      </div>
+    </div>
+@endsection
