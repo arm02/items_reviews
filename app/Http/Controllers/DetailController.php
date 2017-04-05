bf7efdd8d5ef2475a996e62e8e5f7b8bd9371f4b
andy <andy.natalino@gmail.com>
2017-04-05 10:39:42 +0700

    tambahan search pada result barang

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -2,34 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
-use Auth;
 
 class DetailController extends Controller
 {
 
-
-    public function detail($id)
+  public function detail($id)
   {
-  	$data['barang'] = \App\Barang::whereSlug($id)->first();
+    $data['barang'] = \App\Barang::whereSlug($id)->first();
     $data['komentar']=\App\komentar::whereIdArtikel($data['barang']->id)->get();
     return view('barang.detail')->with($data);
- }
+  }
 
-         public function search(Request $request)
-    {
-      $search = $request->get('q');
-      $hasil =  \App\Barang::where('nama_barang', 'LIKE', '%' . $search . '%')->paginate();
-      return view('barang/result', compact('hasil', 'search'));  
-    }
+  public function search(Request $request)
+  {
+    $search = $request->get('q');
+    $hasil =  \App\Barang::where('nama_barang', 'LIKE', '%' . $search . '%')->paginate();
+    return view('barang/result', compact('hasil', 'search'));
+  }
 
 
-    public function all()
-    {
-      $data['barang'] = \App\Barang::paginate(16);
-      return view('barang.all')->with($data);
-    }
+  public function all()
+  {
+    $data['barang'] = \App\Barang::paginate(16);
+    return view('barang.all')->with($data);
+  }
 
 
 }
--- resources/views/barang/result.blade.php
+++ resources/views/barang/result.blade.php
@@ -2,72 +2,77 @@
 
 @section('header')
 
- <title>Laravel &raquo; Home</title>
-    @endsection
+<title>Laravel &raquo; Home</title>
+@endsection
 
-    @section('content')
+@section('content')
 
-    <script type="text/javascript">(function(d, t, e, m){
-    
-    // Async Rating-Widget initialization.
-    window.RW_Async_Init = function(){
-                
-        RW.init({
-            huid: "352418",
-            uid: "d76e769917f42f96dfbd229c445137a5",
-            options: { "style": "oxygen" } 
-        });
-        RW.render();
-    };
-        // Append Rating-Widget JavaScript library.
-    var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
-        l = d.location, ck = "Y" + t.getFullYear() + 
-        "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
-        f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
-        a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
-    if (d.getElementById(id)) return;              
-    rw = d.createElement(e);
-    rw.id = id; rw.async = true; rw.type = "text/javascript";
-    rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
-    s.parentNode.insertBefore(rw, s);
-    }(document, new Date(), "script", "rating-widget.com/"));</script>
+<script type="text/javascript">(function(d, t, e, m){
 
+  // Async Rating-Widget initialization.
+  window.RW_Async_Init = function(){
+
+    RW.init({
+      huid: "352418",
+      uid: "d76e769917f42f96dfbd229c445137a5",
+      options: { "style": "oxygen" }
+    });
+    RW.render();
+  };
+  // Append Rating-Widget JavaScript library.
+  var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
+  l = d.location, ck = "Y" + t.getFullYear() +
+  "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
+  f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
+  a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
+  if (d.getElementById(id)) return;
+  rw = d.createElement(e);
+  rw.id = id; rw.async = true; rw.type = "text/javascript";
+  rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
+  s.parentNode.insertBefore(rw, s);
+}(document, new Date(), "script", "rating-widget.com/"));</script>
+
+<form action={{ url('search') }} method="GET" class="form-signin">
 <div class="row text-center">
-<div class="col-md-12">
-<input class="form-control" style="width:30%;margin-left:27%" type="text">
-<button class="btn btn-md btn-primary" style="margin-top:-4.8%;margin-left:20%">Go !</button>
+  <div class="col-md-12">
+    <input class="form-control" name="q" style="width:30%;margin-left:27%" type="text">
+    <button class="btn btn-md btn-primary" style="margin-top:-4.8%;margin-left:20%">Go !</button>
+  </div>
 </div>
-</div>
-<br>
+</form>
 @if (count($hasil))
+<div class="page-header">
+  <div class="alert alert-success" role="alert">Hasil pencarian : <b>{{$search}}</b></div>
+</div><br>
+
 <div class="container">
-<div class="row">
-@foreach($hasil as $data)
-  <div class="col-md-3">
-    <div class="thumbnail" style="height: 400px;">
+  <div class="row">
+    @foreach($hasil as $data)
+    <div class="col-md-4">
+      <div class="thumbnail">
         <div align="center">
-        <h5><font color="red"> {{ $data->asal }}</h5></font>
-        <h6>Posted by {{\App\User::find($data->id_user)['name']}}</h6>
-        <hr>
-        </div>  
-      <img style="height: 150px;" src="{{url('images/'.$data->sampul)}}">
-      <div class="caption" style="height: 60px">
-        <h6>{{ $data->nama_barang }}</h6>
-        <h5>Rp.{{ $data->harga }} </h5>
+          <h5><font color="red"> {{ $data->asal }}</h5></font>
+          <h6>Posted by {{\App\User::find($data->id_user)['name']}}</h6>
+          <hr>
+        </div>
+        <img style="height: 150px;" src="{{url('images/'.$data->sampul)}}">
+        <div class="caption">
+          <div class="rw-ui-container" align="right"></div>
+          <h6>{{ $data->nama_barang }}</h6>
+          <h5>Rp.{{ $data->harga }} </h5>
+          <br>
+          <div align="center">
+            <p><a href="{{url('/'.$data->slug)}}" class="btn btn-primary" role="button">Read More</a></p>
+          </div>
+        </div>
       </div>
-        <hr>
-        <div align="center" style="position: relative;">
-        <p><a href="{{url('/'.$data->slug)}}" class="btn btn-primary" role="button">Read More</a></p>
-      </div>
-        <br>
     </div>
+    @endforeach
   </div>
-@endforeach
-  </div>
-</div> 
+</div>
 @else
-	<center>
-   <div class="alert alert-danger"> Oops.. Data <b>{{$search}}</b> Tidak Ditemukan</div>
-   </center>
+<center>
+  <div class="alert alert-danger"> Oops.. Data <b>{{$search}}</b> Tidak Ditemukan</div>
+</center>
 @endif
-	   @endsection
+@endsection
