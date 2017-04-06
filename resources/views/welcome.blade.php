191f73945c97567270ea478295028545374b7feb
Hafizh Hi-Fiez <Hafizh Hi-Fiez>
2017-04-06 13:30:55 +0700

    welcome responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,16 +14,22 @@
     padding: 10px;
 }
 .hafizh {
-    position: absolute;
+  width: 960px;
+  margin:0 auto;
+line-height: 1.4em;
+  position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+@media only screen and (max-width: 479px){
+    .hafizh { width: 90%; }
 }
     </style>
     <div class="container">
     <div class="row">
     <main>
-    <div class="hafizh">
+    <div class="hafizh col-md-12">
     <center>
     <img src="{{url('images/google-web-search-xxl.png')}}" width="200" height="">
     </center>
@@ -54,29 +60,26 @@
       </form>
       </div>
       </div>
-      </div> -->
-
-      <div class="container">
-               <div class="row">
+      </div> -->      
                  <form action="{{ url('search') }}" method="GET" class="form-signin">
-                   <div class="center" style="width:500px; margin:0 auto;">
-                     <center>
-                       <input type="text" name="query" class="form-control" placeholder="Search Here" required autofocus>
-                       <div class="input-group"><br>
-                         <select class="btn btn-primary" name="category" required>
-                           <option value="">- Cari online shop -</option>
+                   <div class="center" style="width:65%; margin:0 auto;">
+                     <center>                       
+                       <div style="display: inline-flex;" class="input-group col-md-12">                        
+                       <select class="btn btn-primary pull-left" name="category" required>
+                           <option class="btn-primary" value="">Categories</option>
                            @foreach($category as $data)
-                           <option value="{{ $data->nama_category }}  "> {{ $data->nama_category }}</option>
+                           <option class="btn-primary" value="{{ $data->nama_category }}  "> {{ $data->nama_category }}</option>
                            @endforeach
-                         </select>
-                       </div>
-                     </center>
-                     <center>
-                       <br>
+                         </select>                                   
+                         <input type="text" name="query" class="form-control" placeholder="Search Here" required autofocus >
+                       </div>                        
+                       <p></p>
                        <button class="btn btn-lg btn-primary" type="submit">Search</button>
-                     </center>
-                   </form>
+                     </center>                   
                  </div>
+                 </form>
+                 </div>
+                 </main>
                </div>
              </div>
 
